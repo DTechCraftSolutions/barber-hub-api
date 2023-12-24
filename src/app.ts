561eeefd1ec21cb270d666c7df5c99b2763b43c0
DeyvisonTav <deyvisontav@gmail.com>
2023-12-24 20:03:28 -0300

    feat: added routes for professionals

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import { usersRoutes } from "./http/controllers/users/routes";
 import { barberRoutes } from "./http/controllers/barber/routes";
 import { serviceRoutes } from "./http/controllers/service/routes";
 import { rateRoutes } from "./http/controllers/rate/routes";
+import { professionalRoutes } from "./http/controllers/professional/routes";
 export const app = fastify();
 
 app.register(usersRoutes);
 app.register(barberRoutes);
 app.register(serviceRoutes);
 app.register(rateRoutes);
+app.register(professionalRoutes);
